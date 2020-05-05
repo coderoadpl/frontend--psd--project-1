c71aba6b5ddad6ae720a661991869adac8866f54
Mateusz Choma <kontakt@coderoad.pl>
2020-05-05 06:05:44 +0200

    rendering elements

--- script.js
+++ script.js
@@ -1,9 +1,97 @@
-let names = []
+let names = ['Ala', 'Ela']
 
 const addName = function (newName) {
     names = names.concat(newName)
 }
 
-const nameExists = function (name) { 
+const nameExists = function (name) {
     return names.includes(name)
 }
+
+const renderList = function () {
+
+    const ul = document.createElement('ul')
+
+    for (let i = 0; i < names.length; i++) {
+
+        const li = document.createElement('li')
+
+        li.innerText = names[i]
+
+        ul.appendChild(li)
+    }
+
+    return ul
+
+}
+
+const renderNewNameInput = function () {
+
+    const div = document.createElement('div')
+
+    const input = document.createElement('input')
+    const button = document.createElement('button')
+
+    input.setAttribute('placeholder', 'Add new name')
+    button.innerText = 'ADD'
+
+    div.appendChild(input)
+    div.appendChild(button)
+
+    return div
+
+}
+
+const renderSearchInput = function () {
+
+    const div = document.createElement('div')
+
+    const input = document.createElement('input')
+
+    input.setAttribute('placeholder', 'Search name')
+
+    div.appendChild(input)
+
+    return div
+
+}
+
+const renderSearchResult = function () {
+
+    const p = document.createElement('p')
+
+    p.innerText = 'Result'
+
+    return p
+
+}
+
+const render = function () {
+
+    const div = document.createElement('div')
+
+    const list = renderList()
+    const newNameInput = renderNewNameInput()
+    const searchInput = renderSearchInput()
+    const searchResult = renderSearchResult()
+
+    div.appendChild(list)
+    div.appendChild(newNameInput)
+    div.appendChild(searchInput)
+    div.appendChild(searchResult)
+
+    return div
+
+}
+
+const init = function (containerSelector) {
+    const container = document.querySelector(containerSelector)
+
+    if (!container) return
+
+    const app = render()
+
+    container.appendChild(app)
+}
+
+init('body')
